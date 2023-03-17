3cd54b03a73fab263fcf142072efad9a3a68a559
parmarshubham305 <parmarshubham305@gmail.com>
2023-03-18 02:14:21 +0530

    Registration page and js  updated

--- public/front/js/theme.js
+++ public/front/js/theme.js
@@ -94,3 +94,45 @@ $(function () {
     });
 });
 
+// registration Js
+$(document).ready(function () {
+    var navListItems = $('ul.setup-panel li a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-select').addClass('btn-default');
+            $item.addClass('btn-select').parent().addClass("active");
+            allWells.hide();
+            $target.show().addClass("active");
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('ul.setup-panel li a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('ul.setup-panel li a.btn-select').trigger('click');
+    });
