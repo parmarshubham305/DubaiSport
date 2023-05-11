611b6cb77a854db227f51764da9d259a95887a53
JackyPatel <jackypatel209@gmail.com>
2023-05-11 22:37:57 +0530

    Route issue fix

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/wishlist', [App\Http\Controllers\HomeController::class, 'wishlist'])->name('wishlist');
-
-Route::group(['as' => 'front.', 'namespace' => 'Front'], function () {
-    Route::post('products/delete', 'ProductController@delete')->name('products.delete');
-    Route::resource('/products', 'ProductController');
-    Route::get('/{category_group_slug}/{category_slug}', 'ProductController@index')->name('category.products');
-
-    Route::resource('/cart', 'CartController');
-    Route::resource('/checkout', 'CheckoutController');
-    Route::resource('/orders', 'OrderController');
-});
+if (!\Request::is('admin/*')) {
+    Route::group(['as' => 'front.', 'namespace' => 'Front'], function () {
+        Route::post('products/delete', 'ProductController@delete')->name('products.delete');
+        Route::resource('/products', 'ProductController');
+        Route::get('/{category_group_slug}/{category_slug}', 'ProductController@index')->name('category.products');
+    
+        Route::resource('/cart', 'CartController');
+        Route::resource('/checkout', 'CheckoutController');
+        Route::resource('/orders', 'OrderController');
+    });
+}
